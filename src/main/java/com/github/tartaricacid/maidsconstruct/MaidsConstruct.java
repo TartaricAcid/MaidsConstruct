06eed80859fa31fef643d65b1602c625490cb3e2
tartaric_acid <baka943@qq.com>
2026-03-12 19:43:36 +0800

    修正一处服务端崩溃的问题

--- src/main/java/com/github/tartaricacid/maidsconstruct/MaidsConstruct.java
+++ src/main/java/com/github/tartaricacid/maidsconstruct/MaidsConstruct.java
@@ -4,11 +4,13 @@ import com.github.tartaricacid.maidsconstruct.client.compat.cloth.ClothConfigInt
 import com.github.tartaricacid.maidsconstruct.config.MaidsConstructConfig;
 import com.github.tartaricacid.maidsconstruct.init.InitContainers;
 import com.github.tartaricacid.maidsconstruct.init.InitMemories;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.LoadingModList;
 
 @Mod(MaidsConstruct.MOD_ID)
@@ -27,7 +29,7 @@ public class MaidsConstruct {
         InitContainers.CONTAINER_TYPE.register(modBus);
 
         // 如果 cloth config api 模组存在，注册女仆的配置界面
-        if (LoadingModList.get().getModFileById(CLOTH_CONFIG) != null) {
+        if (FMLEnvironment.dist == Dist.CLIENT && LoadingModList.get().getModFileById(CLOTH_CONFIG) != null) {
             MinecraftForge.EVENT_BUS.register(new ClothConfigIntegration());
         }
     }
